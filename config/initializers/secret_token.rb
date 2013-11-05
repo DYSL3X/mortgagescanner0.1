172ba5543258b792a9a103558d5fcb45b8514629
Stephen <stephen@gmail.com>
2013-11-05 10:41:45 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mortgagescanner01::Application.config.secret_key_base = '640c132282db173ae4d8a3c5e5f280c819886d184a49439559948c2912a0da6277f4663b07bfe7c7e081a359a3e9b78f1c77a0b516082361a3c80e7f6039d835'
